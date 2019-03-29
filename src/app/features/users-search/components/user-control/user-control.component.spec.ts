839bdf1abd5a523ac6fbd8af8c7fb399b7971e90
Vynnychuk <TCVynnychuk@kiev.luxoft.com>
2019-03-29 12:38:59 +0200

    implement unit tests for search-bar, user-control, users-list components

--- src/app/features/users-search/components/user-control/user-control.component.spec.ts
+++ src/app/features/users-search/components/user-control/user-control.component.spec.ts
@@ -1,16 +1,33 @@
+import { ChangeDetectionStrategy } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { UserControlComponent } from './user-control.component';
 
 describe('UserControlComponent', () => {
+  const selectors = {
+    label: 'label.col-form-label',
+    controlWrapper: '.control',
+    controlInput: '.control input',
+    controlLink: '.control a'
+  };
   let component: UserControlComponent;
   let fixture: ComponentFixture<UserControlComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserControlComponent ]
+      declarations: [UserControlComponent]
     })
-    .compileComponents();
+    // Need to override detection strategy due to existing angular issue:
+    // https://github.com/angular/angular/issues/12313#issuecomment-298697327
+      .overrideComponent(
+        UserControlComponent,
+        {
+          set: {
+            changeDetection: ChangeDetectionStrategy.Default
+          }
+        }
+      )
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,8 +36,108 @@ describe('UserControlComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component)
-      .toBeTruthy();
+  describe('component View', () => {
+    it('should create', () => {
+      expect(component)
+        .toBeTruthy();
+    });
+
+    describe('label', () => {
+      it('should display the same value as input', () => {
+        component.label = 'fakeLabel';
+        fixture.detectChanges();
+
+        const labelElement = fixture.debugElement.query(By.css(selectors.label)).nativeElement;
+
+        expect(labelElement.textContent.trim())
+          .toBe('fakeLabel');
+      });
+    });
+
+    describe('text input', () => {
+      it('should be shown in case "value" input has been specified', () => {
+        component.value = 'fakeValue';
+        fixture.detectChanges();
+
+        const input = fixture.debugElement.query(By.css(selectors.controlInput)).nativeElement;
+
+        expect(input)
+          .toBeTruthy();
+      });
+
+      it('should not be shown in case "value" input has not been specified', () => {
+        component.value = undefined;
+        fixture.detectChanges();
+
+        const input = fixture.debugElement.query(By.css(selectors.controlInput));
+
+        expect(input)
+          .toBeNull();
+      });
+
+      it('should display the same value as input', () => {
+        component.value = 'fakeValue';
+        fixture.detectChanges();
+
+        const input = fixture.debugElement.query(By.css(selectors.controlInput)).nativeElement;
+
+        expect(input.value)
+          .toBe('fakeValue');
+      });
+    });
+
+    describe('link', () => {
+      it('should be shown in case "href" input has been specified', () => {
+        component.href = 'fakeHref';
+        fixture.detectChanges();
+
+        const link = fixture.debugElement.query(By.css(selectors.controlLink)).nativeElement;
+
+        expect(link)
+          .toBeTruthy();
+      });
+
+      it('should not be shown in case "href" input has not been specified', () => {
+        component.href = undefined;
+        fixture.detectChanges();
+
+        const link = fixture.debugElement.query(By.css(selectors.controlLink));
+
+        expect(link)
+          .toBeNull();
+      });
+
+      it('should display the same value as input', () => {
+        component.href = 'fakeHref';
+        fixture.detectChanges();
+
+        const link = fixture.debugElement.query(By.css(selectors.controlLink));
+
+        expect(link.properties.href)
+          .toBe('fakeHref');
+      });
+    });
+
+    describe('control wrapper', () => {
+      it('should apply label css class in case href input has been specified', () => {
+        component.href = 'fakeHref';
+        fixture.detectChanges();
+
+        const controlWrapper = fixture.debugElement.query(By.css(selectors.controlWrapper)).nativeElement as HTMLElement;
+
+        expect(controlWrapper.classList.contains('col-form-label'))
+          .toBeTruthy();
+      });
+
+      it('should not apply label css class in case href input has not been specified', () => {
+        component.href = undefined;
+        fixture.detectChanges();
+
+        const controlWrapper = fixture.debugElement.query(By.css(selectors.controlWrapper)).nativeElement as HTMLElement;
+
+        expect(controlWrapper.classList.contains('col-form-label'))
+          .toBeFalsy();
+      });
+    });
   });
 });
--- src/app/testing/mocks/mocks.module.ts
+++ src/app/testing/mocks/mocks.module.ts
@@ -2,20 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Store, StoreModule } from '@ngrx/store';
 import { MockState, MockStore } from '@ngrx/store/testing';
-import {
-  MockNgbAccordionComponent,
-  MockNgbPanelComponent,
-  MockSearchBarComponent,
-  MockUserControlComponent,
-  MockUsersListComponent
-} from './components';
+import { MockSearchBarComponent, MockUserControlComponent, MockUsersListComponent } from './components';
 
 const components = [
   MockSearchBarComponent,
   MockUserControlComponent,
   MockUsersListComponent,
-  MockNgbAccordionComponent,
-  MockNgbPanelComponent
 ];
 
 @NgModule({
